c64abe4a3931aa70604fd56b96694e2ad5fb9320
RomanHalaburda <mtbrider94@gmail.com>
2017-04-18 11:00:36 +0300

    Changed BallanceAbstract.php

--- BallanceAbstract.php
+++ BallanceAbstract.php
@@ -6,12 +6,62 @@
  * Date: 18.04.2017
  * Time: 3:10
  */
-class IBallanceDebet
+
+abstract class ABallance
+{
+    protected $ballance;
+    protected $percent;
+
+    abstract protected function getPercent(); // вывести процент
+    abstract protected function doPercent(); // отнять/прибавить процент к имеющемуся счёту
+    abstract protected function getBallance(); // вывести баланс
+    abstract protected function setBallance($_b); // задать баланс
+
+    // общий конструктор класса
+    public function ABallance($_b, $_p)
+    {
+        $this->ballance = $_b;
+        $this->percent = $_p;
+    }
+}
+
+class ABallanceDebet extends ABallance
+{
+    public function ABallanceDebet($_b, $_p)
+    {
+        $this->ABallance($_b, $_p);
+    }
+
+    protected function getPercent() { return $this->percent; }
+    protected function getBallance() { return $this->ballance; }
+    protected function setBallance($_b) { $this->ballance = $_b; }
+    protected function doPercent()
+    {
+        // TODO: Implement doPercent() method.
+
+    }
+
+    public function manyRequest ($_n) // снятие денег со счета
+    {
+        echo $this->getBallance().'  '.$this->getPercent();
+    }
+
+    public function manyAdd($_n) // взнос денег на счет
+    {
+        echo $this->getBallance().'  '.$this->getPercent();
+    }
+}
+
+class ABallanceCredit
 {
 
 }
 
-class IBallanceCredit
-{
+// делаем разметку
+echo '<html><head><title>Abstract Ballance</title></head><body>';
 
-}
+$bDebet = new ABallanceDebet(5000, 0.5);
+echo $bDebet->manyAdd(5);
+
+// завершаем разаметку
+echo '</body></html>';
